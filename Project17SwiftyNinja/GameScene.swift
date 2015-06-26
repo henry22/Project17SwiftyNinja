7393aedb17b9b9a9613cb4b2a4057e880dc2fab0
henry22 <henry811010@gmail.com>
2015-06-26 21:50:03 +0800

    Set a background and add a gravity.

--- Project17SwiftyNinja/GameScene.swift
+++ Project17SwiftyNinja/GameScene.swift
@@ -10,33 +10,23 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        let background = SKSpriteNode(imageNamed: "sliceBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .Replace
+        addChild(background)
         
-        self.addChild(myLabel)
+        //the vector arrow is pointing straight down
+        physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+        //stay up in the air a bit longer
+        physicsWorld.speed = 0.85
+        
+//        createScore()
+//        createLives()
+//        createSlices()
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-        /* Called when a touch begins */
         
-        for touch in (touches as! Set<UITouch>) {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
     }
    
     override func update(currentTime: CFTimeInterval) {
